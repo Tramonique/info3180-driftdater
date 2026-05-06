0e85d22050595eb9c1fe49cadd9b5fc6520aee24
Britnie-Ann Gray <britnieanngrayy@gmail.com>
2026-05-06 12:45:28 -0500

    Routing for message page added to matches pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import RegisterView from '../views/RegisterView.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Matches from '../views/Matches.vue'
 import Profile from '../views/Profile.vue'
+import MessageCenter from '../views/MessageCenter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,6 +48,11 @@ const router = createRouter({
       //Routing to create user profile
       path: '/createprofile',
       component: Profile
+    },
+    {
+      path: 'message/:matchID/:receiverID',
+      name: 'message',
+      component: MessageCenter
     }
   ]
 })
--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,26 +1,40 @@
 <script setup>
 import { ref, onMounted} from "vue"
+import { useRouter } from "vue-router"
 
 const matches = ref([])
 const loading = ref(false)
-const erros = ref(null)
+const error = ref(null)
+const router = useRouter()
 
+//Loading of all matched profiles
 const fetchMatches = async() => {
     loading.value = true
     error.value = null
     try{
-        const response = await fetch('http://localhost:8000/api/matches/')
+        const response = await fetch('/api/matches/')
         if (!response.ok){
             throw new Error('Failed to fetch matched profiles')
         }
         
         const data = await response.json()
-        matches.value = data
+        matches.value = data.matches
     } catch (err){
         error.value - err.message
     } finally {
         loading.value = false
     }
+}
+
+//Message navigation
+const goToMessages = (match) => {
+    router.push({
+        name: 'message',
+        params:{
+            matchID: match.match_id,
+            receiverID: match.user_id
+        }
+    })
 }
 onMounted(fetchMatches)
 
@@ -30,13 +44,15 @@ onMounted(fetchMatches)
 <template>
     <div class="body">
         <h1>Your Matches</h1>
+        <p v-if="loading">Loading...</p>
+        <p v-if="error">{{ error }}</p>
         <ul v-for="match in matches" :key="match_id">
             <div class="matchedProfile">
-                <img :src="match.profile_picture" :alt="match.full_name" class="avatar">
+                <img :src="match.profile_picture" :alt="match.full_name" class="avatar"> <!--if src doesnt load do "/uploads/${match.profile_picture}"-->
                 <div class="info">
                     <h3>{{ match.full_name }}, {{ match.age }}</h3>
                     <p>{{ matches.bio }}</p>
-                    <button> Message </button>
+                    <button @click="goToMessages"> Message </button>
                 </div>
 
             </div>
