89847b862c02238d71c4829368d5708a16d718c4
Britnie-Ann Gray <britnieanngrayy@gmail.com>
2026-05-07 22:52:20 -0500

    search and edit profile added to header

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Matches from '../views/Matches.vue'
 import Profile from '../views/Profile.vue'
 import MessageCenter from '../views/MessageCenter.vue'
 import SearchView from '../views/SearchView.vue'
+import EditProfile from '../views/EditProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -57,6 +58,11 @@ const router = createRouter({
       //Search specific users by a filter
       path: '/search',
       component: SearchView
+    },
+    {
+      //Edit Profile of user
+      path: '/editprofile',
+      component: EditProfile
     }
   ]
 })
